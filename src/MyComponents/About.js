200efe31920d6ee97361d85945bbabf5e390fb3b
Abismruta Sasamal <simunabismruta.2014@gmail.com>
2022-05-25 15:09:11 +0530

    Final Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an obout component.
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ut iure illum totam perferendis molestias cum. Natus, optio. Vel explicabo dolorem excepturi repellat ad vero, atque numquam placeat ea omnis.</p>
+    </div>
+  )
+}
